f38e04222042b50cbbd67836f893b30e36cda4ed
John Okall <Jhnprimee@gmail.com>
2024-11-30 10:07:38 +0300

    add data for media table

---
+++ mockdata/media.sql
@@ -0,0 +1,12 @@
+INSERT INTO Media (post_id, user_id, file_url, file_type, file_size)
+VALUES
+(2, 2, 'https://picsum.photos/200', 'IMAGE', 204800),
+(4, 4, 'https://picsum.photos/200', 'IMAGE', 204800),
+(6, 6, 'https://picsum.photos/200', 'IMAGE', 204800),
+(8, 8, 'https://picsum.photos/200', 'IMAGE', 204800),
+(10, 10, 'https://picsum.photos/200', 'IMAGE', 204800),
+(12, 12, 'https://picsum.photos/200', 'IMAGE', 204800),
+(14, 14, 'https://picsum.photos/200', 'IMAGE', 204800),
+(16, 16, 'https://picsum.photos/200', 'IMAGE', 204800),
+(18, 18, 'https://picsum.photos/200', 'IMAGE', 204800),
+(20, 20, 'https://picsum.photos/200', 'IMAGE', 204800);
